667f2d5efe626b89a753181e426cc05a2c683d8f
rzbrnl <rzbrnl@gmail.com>
2026-07-09 14:43:33 -0700

    fix: usar HIcon en clients para consistencia con dashboard

--- src/views/ClientsView.vue
+++ src/views/ClientsView.vue
@@ -50,28 +50,16 @@
           </div>
           <div class="client-actions">
             <button class="client-action" @click="startNewQuotation(client)" title="Nueva cotización">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <line x1="12" y1="5" x2="12" y2="19"/>
-                <line x1="5" y1="12" x2="19" y2="12"/>
-              </svg>
+              <HIcon name="plus" :size="16" />
             </button>
             <button class="client-action" @click="showClientQuotations(client)" title="Ver cotizaciones">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
-                <polyline points="14 2 14 8 20 8"/>
-              </svg>
+              <HIcon name="document" :size="16" />
             </button>
             <button class="client-action" @click="editClient(client)" title="Editar">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-              </svg>
+              <HIcon name="edit" :size="16" />
             </button>
             <button class="client-action delete" @click="deleteClient(client)" title="Eliminar">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <polyline points="3 6 5 6 21 6"/>
-                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>
-              </svg>
+              <HIcon name="trash" :size="16" />
             </button>
           </div>
         </div>
@@ -182,6 +170,7 @@ import { useToastStore } from '../stores/toast'
 import { useConfirmStore } from '../stores/confirm'
 import { formatCurrency } from '../utils/format'
 import AppLayout from '../components/AppLayout.vue'
+import HIcon from '../components/HIcon.vue'
 
 const router = useRouter()
 const clientsStore = useClientsStore()
